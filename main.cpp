f32696239f4741cf69c4e2f8ec1962a0b4e053b8
Fady Youssef <62950351+Durantulaa@users.noreply.github.com>
2024-04-03 00:08:37 +0000

    test comm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+
+
+struct ListD{
+
+
+deleteNode(itemtype item, int pos){
+doubleNode *tmp = new doubleNode;
+doubleNode *pta = findpos(pos);
+doubleNode *ptb = pta->next;
+ tmp = pta->next;
+}
 int main() {
   std::cout << "Hello World!\n";
 }
