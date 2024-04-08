83cfc773b18ef89fb15197325d0c15a17ef1bc88
Fady Youssef <62950351+Durantulaa@users.noreply.github.com>
2024-04-08 05:21:51 +0000

    Still need to complete sort in selection sort algo

--- .ccls-cache/@home@runner@proj9/mainTest.cpp
+++ .ccls-cache/@home@runner@proj9/mainTest.cpp
@@ -1,68 +1,90 @@
 #include <iostream>
 using namespace std;
 
-#include "main".h"
+#include "main.h"
 
-//All tests are controlled by a dedicated function
+// All tests are controlled by a dedicated function
 void TestInsert();
 void TestCopyConstructor();
+void TestDelete();
 
-int main()
-{
- //TestInsert();
- TestCopyConstructor();
+int main() {
+  TestInsert();
+  TestCopyConstructor();
+  TestDelete();
 }
 
+void TestDelete() {
+  ListD *lst2 = new ListD;
+  // int i=0;
+  // while ( i <= 10){
+  //   lst2->Insert(i, i);
+  //   i++;
+  // }
 
+  cout << "Passed if  0, 50, and 100 get deleted from the list \n" << endl;
+  lst2->Delete(50);
+  lst2->Delete(100);
+  lst2->Delete(0);
+  lst2->PrintForward();
 
-void TestInsert()
-{
+//   cout << "if this works everything in the list will be deleted \n";
+//   while ( i != 10){
+//   lst2->DeleteAll(i);
+//   i++;
+//   }
+//   lst2->PrintForward();
+ } 
 
- ListD* lst = new ListD;
- for (int i = 1; i <= 5; i++)
-  lst->Insert(i,i);
+void TestInsert() {
 
- cout << "test general case insert" << endl;
- cout << "Passed Insert Test 1 if 1 through 5 appear on subsequent lines" << endl;
- lst->PrintForward();
- cout << endl;
+  ListD *lst = new ListD;
+  for (int i = 1; i <= 5; i++)
+    lst->Insert(i, i);
 
- cout << "test insert at the head" << endl;
- lst->Insert(0,1);
- cout << "Passed Insert Test 2 if 0 appears in position 1" << endl; 
- lst->PrintForward();
- cout << endl;
+  cout << "test general case insert \n" << endl;
+  cout << "Passed Insert Test 1 if 1 through 5 appear on subsequent lines \n";
+  lst->PrintForward();
+  cout << endl;
 
- cout << "test insert at the tail" << endl; 
- lst->Insert(100,7);
- cout << "Passed Insert Test 3 if 100 appears in final position" << endl; 
- lst->PrintForward();
- cout << endl;
+  cout << "test insert at the head" << endl;
+  lst->Insert(0, 1);
+  cout << "Passed Insert Test 2 if 0 appears in position 1" << endl;
+  lst->PrintForward();
+  cout << endl;
 
- cout << "test insert within the list" << endl; 
- lst->Insert(50,5);
- cout << "Passed Insert Test 4 if 50 appears in middle position" << endl; 
- lst->PrintForward();
- cout << endl;
- //delete lst;
+  cout << "test insert at the tail" << endl;
+  lst->Insert(100, 7);
+  cout << "Passed Insert Test 3 if 100 appears in final position" << endl;
+  lst->PrintForward();
+  cout << endl;
+
+  cout << "test insert within the list" << endl;
+  lst->Insert(50, 5);
+  cout << "Passed Insert Test 4 if 50 appears in middle position" << endl;
+  lst->PrintForward();
+  cout << endl;
+  // delete lst;
 }
 
-void TestCopyConstructor()
-{
- ListD* lst1 = new ListD;
- for (int i = 1; i <= 3; i++)
-  lst1->Insert(i,i);
+void TestCopyConstructor() {
+  ListD *lst1 = new ListD;
+  for (int i = 1; i <= 3; i++)
+    lst1->Insert(i, i);
 
- ListD* lst2(lst1);
+  ListD *lst2(lst1);
 
- cout << "Test Copy Constructor" << endl; 
- cout << "Test passed if copy is indentical to initial list" << endl; 
- cout << "Traverse inital list" << endl;
- lst1->PrintForward();
- cout << endl;
- cout << "Traverse copy" << endl;
- lst2->PrintForward();
+  cout << "Test Copy Constructor" << endl;
+  cout << "Test passed if copy is indentical to initial list" << endl;
+  cout << "Traverse inital list: " << endl;
+  lst1->PrintForward();
+  cout << endl;
+  cout << "Traverse copy: " << endl;
+  lst2->PrintForward();
+  cout << endl;
+  cout << "Reveresed Copy: " << endl;
+  lst2->PrintBackward();
 
- //delete lst1;
- //delete lst2;
+   // delete lst1;
+   // delete lst2;
 }
--- main.cpp
+++ main.cpp
@@ -1,94 +1,165 @@
-#include <iostream>
+/*
+Team Members: Steph Borla , Daisy , And Fady Youssef
+Course: CPSC122
+Date Submitted: 4/07/2024
+Assignment Name: Project 9
+*/
+
 #include <climits>
+#include <iostream>
 using namespace std;
 
 #include "main.h"
 
-ListD::ListD()
+ListD::ListD() { InitializeVars(); } // done
+
+ListD::~ListD() {
+  // Delete every node in list
+  doubleNode *curr = head->next;
+  doubleNode *tmp = curr;
+
+  while (curr != tail) {
+    curr = curr->next;
+    delete tmp;
+  }
+
+  delete tail;
+  delete head;
+  length = 0;
+
+  cout << "destructor called \n";
+} // done
+
+void ListD::InitializeVars() {
+  length = 0;
+
+  // create dummy nodes;
+  head = new doubleNode;
+  tail = new doubleNode;
+
+  // set values for head dummy node;
+  head->prev = NULL;
+  head->item = INT_MIN;
+  head->next = tail;
+
+  // set values for tail dummy node;
+  tail->prev = head;
+  tail->item = INT_MAX;
+  tail->next = NULL;
+} // done
+
+ListD::ListD(ListD *lst) {
+  InitializeVars();
+  // returns a pointer to the first node, which is what we want here
+  doubleNode *cur = lst->FindPosition(2);
+  for (int i = 1; i <= lst->length; i++) {
+    cout << cur->item << endl;
+    Insert(cur->item, i);
+    cur = cur->next;
+  }
+} // done
+
+doubleNode *ListD::FindPosition(int pos) {
+  // Inserting at the tail is a special case.  It can be made much more
+  // efficient than this.
+
+  // if (pos <= 1 || pos > length){
+  //   cout << "invalid pos \n";
+  //   return nullptr;
+  // }
+
+  doubleNode *cur = head;
+  int i = 0; // begin at the dummy node
+  while (i < pos - 1) {
+    cur = cur->next;
+    i++;
+  }
+  return cur;
+} // done
+
+void ListD::Insert(itemType item, int pos) {
+  doubleNode *tmp = new doubleNode;
+  // new node goes between these two nodes
+  doubleNode *insertPtA = FindPosition(pos);
+  doubleNode *insertPtB = insertPtA->next;
+
+  tmp->item = item;
+  insertPtA->next = tmp;
+  insertPtB->prev = tmp;
+  tmp->prev = insertPtA;
+  tmp->next = insertPtB;
+
+  // set pointers for nodes before and after the insertion point
+  //  insertPtA -> next = tmp;
+  //  insertPtB -> prev = tmp;
+  length++;
+} // done
+
+void ListD::PrintForward() {
+  doubleNode *cur = head->next;
+
+  int i = 0;
+  while (i < length) {
+    cout << cur->item << endl;
+    cur = cur->next;
+    i++;
+  }
+  cout << endl;
+
+} // done
+
+void ListD::PrintBackward() {
+  doubleNode *cur = tail->prev;
+
+  int i = 0;
+  while (i < length) {
+    cout << cur->item << endl;
+    cur = cur->prev;
+    i++;
+  }
+  cout << endl;
+} // done
+
+void ListD::Delete(int pos) {
+  doubleNode *ptA = FindPosition(pos);
+  doubleNode *delptB = ptA->next;
+  doubleNode *ptC = delptB->next;
+
+  ptA->next = ptC;
+  ptC->prev = ptA;
+  delete delptB;
+  length--;
+} // done
+
+int ListD::DeleteAll(itemType item)
 {
- InitializeVars();
-}
+  int count = 0;
+  doubleNode *cur = head->next;
 
-ListD::ListD(ListD* lst)
+  while(cur != tail) {
+    if(cur->item == item) {
+      doubleNode *tmp = cur;
+      cur = cur->next;
+      delete tmp;
+      count++;
+      length--;
+    }
+    else {
+      cur = cur->next;
+    }      
+    }
+
+   return count; 
+  } //done
+
+void ListD::Sort()
 {
- InitializeVars();
- //returns a pointer to the first node, which is what we want here
- doubleNode* cur = lst->FindPosition(2);
- for (int i = 1; i <= lst->length; i++)
- {
-  cout << cur->item << endl;
-  Insert(cur->item,i);
-  cur = cur->next;
- }
-}
-
-void ListD::InitializeVars()
-{
- length = 0;
-
-//create dummy nodes;
- head = new doubleNode;
- tail = new doubleNode;
-
- //set values for head dummy node;
- head->prev = NULL;
- head->item = INT_MIN;
- head->next = tail;
-
- //set values for tail dummy node;
- tail->prev = head; 
- tail->item = INT_MAX;
- tail->next = NULL;
-}
-
-//how would you do this?
-ListD::~ListD()
-{
-}
-
-doubleNode* ListD::FindPosition(int pos)
-{
- //Inserting at the tail is a special case.  It can be made much more efficient than
- //this.
- doubleNode* cur = head;
- int i = 0;  //begin at the dummy node
- while (i < pos - 1)
- {
-  cur = cur->next;
-  i++;
- }
- return cur;
+  
+  
+  
+  
+  //selection sort 
 } 
 
-void ListD::Insert(itemType item, int pos)
-{
- //new node goes between these two nodes
- doubleNode* insertPtA = FindPosition(pos);  
- doubleNode* insertPtB = insertPtA->next; 
-
- //create new node and set its values
- doubleNode* tmp = new doubleNode; 
- tmp->prev = insertPtA;
- tmp->item = item;
- tmp->next = insertPtB;
-
- //set pointers for nodes before and after the insertion point
- insertPtA->next = tmp;
- insertPtB->prev = tmp;
-
- length++;
-}
-
-void ListD::PrintForward()
-{
- doubleNode*  cur = head->next;
-
- int i = 0;
- while (i < length)
- {
-  cout << cur->item << endl;
-  cur = cur->next;
-  i++;
- }
-}
-
+//https://docs.google.com/document/d/189EUPz9t83PKBPAtBK05MzyNtTs74_CLYAPEx-lGeyw/edit
+//https://www.youtube.com/watch?v=2ybLD6_2gKM
